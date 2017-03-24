671202fd3ae1d33c9a7c1412ae2b0936a834036e
Martin Etmajer <martin@etmajer.com>
2017-03-24 21:05:39 +0100

    Refactor test suite for oneagent-64.

--- test/integration/oneagent-64/serverspec/default_spec.rb
+++ test/integration/oneagent-64/serverspec/default_spec.rb
@@ -5,144 +5,192 @@ set :backend, :exec
 
 ENV['HOME'] = '/tmp/kitchen/data'
 
+DT_TENANT = 'ryx70518'
+DT_TENANTTOKEN = 'NkM5fd7JG1Hzmmoh'
+DT_API_TOKEN = 'w89AycHAQByUUfu993UHG'
+DT_AGENT_BASE_URL = 'https://' + DT_TENANT + '.dev.dynatracelabs.com'
+
+def parse_cmd(cmd, opts)
+  result = ''
+
+  unless opts.empty?
+    opts.each do | key, value |
+      result << key.to_s + "="
+
+      unless value.nil?
+        result << "'" + value.to_s + "'"
+      end
+
+      result << ' '
+    end
+  end
+
+  result << cmd
+end
+
 # Test installer with insufficient arguments
 describe command('~/paas-install.sh') do
   its(:stdout) { should eq "help...\n" }
   its(:exit_status) { should eq 1 }
 end
 
-describe command('DT_TENANT= ~/paas-install.sh') do
+opts = { DT_TENANT: nil }
+describe command(parse_cmd('~/paas-install.sh', opts)) do
   its(:stdout) { should eq "help...\n" }
   its(:exit_status) { should eq 1 }
 end
 
-describe command('DT_API_TOKEN= ~/paas-install.sh') do
+opts = { DT_API_TOKEN: nil }
+describe command(parse_cmd('~/paas-install.sh', opts)) do
   its(:stdout) { should eq "help...\n" }
   its(:exit_status) { should eq 1 }
 end
 
-describe command('DT_TENANT= DT_AGENT_BASE_URL= ~/paas-install.sh') do
+opts = { DT_TENANT: nil, DT_AGENT_BASE_URL: nil }
+describe command(parse_cmd('~/paas-install.sh', opts)) do
   its(:stdout) { should eq "help...\n" }
   its(:exit_status) { should eq 1 }
 end
 
 # Test installer with valid DT_TENANT and invalid DT_API_TOKEN
-describe command('DT_TENANT=12345678 DT_API_TOKEN= ~/paas-install.sh') do
+opts = { DT_TENANT: '12345678', DT_API_TOKEN: nil }
+describe command(parse_cmd('~/paas-install.sh', opts)) do
   its(:stderr) { should eq "paas-install.sh: failed to validate DT_API_TOKEN: \n" }
   its(:exit_status) { should eq 1 }
 end
 
-describe command("DT_TENANT=12345678 DT_API_TOKEN='abcdefghijklmnopqrstuvwxyz$' ~/paas-install.sh") do
+opts = { DT_TENANT: '12345678', DT_API_TOKEN: 'abcdefghijklmnopqrstuvwxyz$' }
+describe command(parse_cmd('~/paas-install.sh', opts)) do
   its(:stderr) { should eq "paas-install.sh: failed to validate DT_API_TOKEN: abcdefghijklmnopqrstuvwxyz$\n" }
   its(:exit_status) { should eq 1 }
 end
 
 # Test installer with invalid DT_TENANT and valid DT_API_TOKEN
-describe command("DT_TENANT= DT_API_TOKEN=abcdefghijklmnopqrstuvwxyz ~/paas-install.sh") do
+opts = { DT_TENANT: nil, DT_API_TOKEN: 'abcdefghijklmnopqrstuvwxyz' }
+describe command(parse_cmd('~/paas-install.sh', opts)) do
   its(:stderr) { should eq "paas-install.sh: failed to validate DT_TENANT: \n" }
   its(:exit_status) { should eq 1 }
 end
 
-describe command("DT_TENANT='1234567$' DT_API_TOKEN=abcdefghijklmnopqrstuvwxyz ~/paas-install.sh") do
+opts = { DT_TENANT: '1234567$', DT_API_TOKEN: 'abcdefghijklmnopqrstuvwxyz' }
+describe command(parse_cmd('~/paas-install.sh', opts)) do
   its(:stderr) { should eq "paas-install.sh: failed to validate DT_TENANT: 1234567$\n" }
   its(:exit_status) { should eq 1 }
 end
 
-describe command('DT_TENANT=1234567 DT_API_TOKEN=abcdefghijklmnopqrstuvwxyz ~/paas-install.sh') do
+opts = { DT_TENANT: '1234567', DT_API_TOKEN: 'abcdefghijklmnopqrstuvwxyz' }
+describe command(parse_cmd('~/paas-install.sh', opts)) do
   its(:stderr) { should eq "paas-install.sh: failed to validate DT_TENANT: 1234567\n" }
   its(:exit_status) { should eq 1 }
 end
 
-describe command('DT_TENANT=123456789 DT_API_TOKEN=abcdefghijklmnopqrstuvwxyz ~/paas-install.sh') do
+opts = { DT_TENANT: '123456789', DT_API_TOKEN: 'abcdefghijklmnopqrstuvwxyz' }
+describe command(parse_cmd('~/paas-install.sh', opts)) do
   its(:stderr) { should eq "paas-install.sh: failed to validate DT_TENANT: 123456789\n" }
   its(:exit_status) { should eq 1 }
 end
 
 # Test installer with invalid DT_TENANT and invalid DT_API_TOKEN
-describe command("DT_TENANT= DT_API_TOKEN= ~/paas-install.sh") do
+opts = { DT_TENANT: nil, DT_API_TOKEN: nil }
+describe command(parse_cmd('~/paas-install.sh', opts)) do
   its(:stderr) { should eq "paas-install.sh: failed to validate DT_TENANT: \n" }
   its(:exit_status) { should eq 1 }
 end
 
-describe command("DT_TENANT='1234567$' DT_API_TOKEN='abcdefghijklmnopqrstuvwxy$' ~/paas-install.sh") do
+opts = { DT_TENANT: '1234567$', DT_API_TOKEN: 'abcdefghijklmnopqrstuvwxy$' }
+describe command(parse_cmd('~/paas-install.sh', opts)) do
   its(:stderr) { should eq "paas-install.sh: failed to validate DT_TENANT: 1234567$\n" }
   its(:exit_status) { should eq 1 }
 end
 
 # Test installer with invalid DT_AGENT_BASE_URL and valid DT_API_TOKEN
-describe command("DT_AGENT_BASE_URL= DT_API_TOKEN=abcdefghijklmnopqrstuvwxyz ~/paas-install.sh") do
+opts = { DT_AGENT_BASE_URL: nil, DT_API_TOKEN: 'abcdefghijklmnopqrstuvwxyz' }
+describe command(parse_cmd('~/paas-install.sh', opts)) do
   its(:stderr) { should eq "paas-install.sh: failed to validate DT_AGENT_BASE_URL: \n" }
   its(:exit_status) { should eq 1 }
 end
 
-describe command('DT_AGENT_BASE_URL=123abc DT_API_TOKEN=abcdefghijklmnopqrstuvwxyz ~/paas-install.sh') do
+opts = { DT_AGENT_BASE_URL: '123abc', DT_API_TOKEN: 'abcdefghijklmnopqrstuvwxyz' }
+describe command(parse_cmd('~/paas-install.sh', opts)) do
   its(:stderr) { should eq "paas-install.sh: failed to validate DT_AGENT_BASE_URL: 123abc\n" }
   its(:exit_status) { should eq 1 }
 end
 
-describe command("DT_AGENT_BASE_URL='http://12345678.live.dynatrace.com' DT_API_TOKEN=abcdefghijklmnopqrstuvwxyz ~/paas-install.sh") do
+opts = { DT_AGENT_BASE_URL: 'http://12345678.live.dynatrace.com', DT_API_TOKEN: 'abcdefghijklmnopqrstuvwxyz' }
+describe command(parse_cmd('~/paas-install.sh', opts)) do
   its(:stderr) { should eq "paas-install.sh: failed to validate DT_AGENT_BASE_URL: http://12345678.live.dynatrace.com\n" }
   its(:exit_status) { should eq 1 }
 end
 
 # Test installer with invalid DT_AGENT_BITNESS
-describe command('DT_AGENT_BITNESS=128 DT_TENANT=12345678 DT_API_TOKEN=abcdefghijklmnopqrstuvwxyz ~/paas-install.sh') do
+opts = { DT_AGENT_BITNESS: '128', DT_TENANT: '12345678', DT_API_TOKEN: 'abcdefghijklmnopqrstuvwxyz' }
+describe command(parse_cmd('~/paas-install.sh', opts)) do
   its(:stderr) { should eq "paas-install.sh: failed to validate DT_AGENT_BITNESS: 128\n" }
   its(:exit_status) { should eq 1 }
 end
 
-describe command('DT_AGENT_BITNESS=sixty-four DT_TENANT=12345678 DT_API_TOKEN=abcdefghijklmnopqrstuvwxyz ~/paas-install.sh') do
+opts = { DT_AGENT_BITNESS: 'sixty-four', DT_TENANT: '12345678', DT_API_TOKEN: 'abcdefghijklmnopqrstuvwxyz' }
+describe command(parse_cmd('~/paas-install.sh', opts)) do
   its(:stderr) { should eq "paas-install.sh: failed to validate DT_AGENT_BITNESS: sixty-four\n" }
   its(:exit_status) { should eq 1 }
 end
 
 # Test installer with invalid DT_AGENT_FOR
-describe command('DT_AGENT_FOR=everything-under-the-sun DT_TENANT=12345678 DT_API_TOKEN=abcdefghijklmnopqrstuvwxyz ~/paas-install.sh') do
+opts = { DT_AGENT_FOR: 'everything-under-the-sun', DT_TENANT: '12345678', DT_API_TOKEN: 'abcdefghijklmnopqrstuvwxyz' }
+describe command(parse_cmd('~/paas-install.sh', opts)) do
   its(:stderr) { should eq "paas-install.sh: failed to validate DT_AGENT_FOR: everything-under-the-sun\n" }
   its(:exit_status) { should eq 1 }
 end
 
 # Test installer with invalid DT_AGENT_PREFIX_DIR
-describe command("DT_AGENT_PREFIX_DIR='a' DT_TENANT=12345678 DT_API_TOKEN=abcdefghijklmnopqrstuvwxyz ~/paas-install.sh") do
+opts = { DT_AGENT_PREFIX_DIR: 'a', DT_TENANT: '12345678', DT_API_TOKEN: 'abcdefghijklmnopqrstuvwxyz' }
+describe command(parse_cmd('~/paas-install.sh', opts)) do
   its(:stderr) { should eq "paas-install.sh: failed to validate DT_AGENT_PREFIX_DIR: a\n" }
   its(:exit_status) { should eq 1 }
 end
 
-describe command("DT_AGENT_PREFIX_DIR='a/b' DT_TENANT=12345678 DT_API_TOKEN=abcdefghijklmnopqrstuvwxyz ~/paas-install.sh") do
+opts = { DT_AGENT_PREFIX_DIR: 'a/b', DT_TENANT: '12345678', DT_API_TOKEN: 'abcdefghijklmnopqrstuvwxyz' }
+describe command(parse_cmd('~/paas-install.sh', opts)) do
   its(:stderr) { should eq "paas-install.sh: failed to validate DT_AGENT_PREFIX_DIR: a/b\n" }
   its(:exit_status) { should eq 1 }
 end
 
-describe command("DT_AGENT_PREFIX_DIR='a//b' DT_TENANT=12345678 DT_API_TOKEN=abcdefghijklmnopqrstuvwxyz ~/paas-install.sh") do
+opts = { DT_AGENT_PREFIX_DIR: 'a//b', DT_TENANT: '12345678', DT_API_TOKEN: 'abcdefghijklmnopqrstuvwxyz' }
+describe command(parse_cmd('~/paas-install.sh', opts)) do
   its(:stderr) { should eq "paas-install.sh: failed to validate DT_AGENT_PREFIX_DIR: a//b\n" }
   its(:exit_status) { should eq 1 }
 end
 
-describe command("DT_AGENT_PREFIX_DIR='here' DT_TENANT=12345678 DT_API_TOKEN=abcdefghijklmnopqrstuvwxyz ~/paas-install.sh") do
+opts = { DT_AGENT_PREFIX_DIR: 'here', DT_TENANT: '12345678', DT_API_TOKEN: 'abcdefghijklmnopqrstuvwxyz' }
+describe command(parse_cmd('~/paas-install.sh', opts)) do
   its(:stderr) { should eq "paas-install.sh: failed to validate DT_AGENT_PREFIX_DIR: here\n" }
   its(:exit_status) { should eq 1 }
 end
 
 # Test installer with non-existent DT_TENANT
-describe command("DT_TENANT=12345678 DT_API_TOKEN=abcdefghijklmnopqrstuvwxyz ~/paas-install.sh") do
+opts = { DT_TENANT: '12345678', DT_API_TOKEN: 'abcdefghijklmnopqrstuvwxyz' }
+describe command(parse_cmd('~/paas-install.sh', opts)) do
   its(:stderr) { should match /^Connecting to 12345678.live.dynatrace.com.*404 Not Found\n$/m }
   its(:exit_status) { should eq 1 }
 end
 
 # Test installer with non-existent DT_AGENT_BASE_URL
-describe command("DT_AGENT_BASE_URL=https://my-tenant.my-cluster.com DT_API_TOKEN=abcdefghijklmnopqrstuvwxyz ~/paas-install.sh") do
+opts = { DT_AGENT_BASE_URL: 'https://my-tenant.my-cluster.com', DT_API_TOKEN: 'abcdefghijklmnopqrstuvwxyz' }
+describe command(parse_cmd('~/paas-install.sh', opts)) do
   its(:stderr) { should match /^Connecting to my-tenant.my-cluster.com.*404 Not Found\n$/m }
   its(:exit_status) { should eq 1 }
 end
 
 # Test installer with invalid DT_API_TOKEN
-describe command("DT_AGENT_BASE_URL='https://ryx70518.dev.dynatracelabs.com' DT_API_TOKEN=abcdefghijklmnopqrstuvwxyz ~/paas-install.sh") do
+opts = { DT_AGENT_BASE_URL: DT_AGENT_BASE_URL, DT_API_TOKEN: 'abcdefghijklmnopqrstuvwxyz' }
+describe command(parse_cmd('~/paas-install.sh', opts)) do
   its(:stderr) { should match /^Connecting to ryx70518.dev.dynatracelabs.com.*401 Unauthorized\n$/m }
   its(:exit_status) { should eq 1 }
 end
 
 # Test installer defaults: all technologies, 64-bit into /var/lib
-describe command("DT_AGENT_BASE_URL='https://ryx70518.dev.dynatracelabs.com' DT_API_TOKEN=w89AycHAQByUUfu993UHG ~/paas-install.sh") do
+opts = { DT_AGENT_BASE_URL: DT_AGENT_BASE_URL, DT_API_TOKEN: 'w89AycHAQByUUfu993UHG' }
+describe command(parse_cmd('~/paas-install.sh', opts)) do
   its(:stdout) { should match /Installing to \/var\/lib.*Unpacking complete./m }
   its(:stderr) { should match /^Connecting to ryx70518.dev.dynatracelabs.com./m }
   its(:exit_status) { should eq 0 }
@@ -150,8 +198,8 @@ end
 
 describe file('/var/lib/dynatrace/oneagent/dynatrace-env.sh') do
   it { should be_file }
-  its(:content) { should include 'export DT_TENANT=ryx70518' }
-  its(:content) { should include 'export DT_TENANTTOKEN=NkM5fd7JG1Hzmmoh' }
+  its(:content) { should include 'export DT_TENANT=' + DT_TENANT }
+  its(:content) { should include 'export DT_TENANTTOKEN=' + DT_TENANTTOKEN }
   its(:content) { should match /export DT_CONNECTION_POINT=".+"/ }
 end
 
@@ -165,7 +213,8 @@ describe file('/tmp/dynatrace-oneagent.sh') do
 end
 
 # Test installer: all technologies, 32/64-bit into /tmp)
-describe command("DT_AGENT_BASE_URL='https://ryx70518.dev.dynatracelabs.com' DT_API_TOKEN=w89AycHAQByUUfu993UHG DT_AGENT_BITNESS=all DT_AGENT_PREFIX_DIR=/tmp ~/paas-install.sh") do
+opts = { DT_AGENT_BASE_URL: DT_AGENT_BASE_URL, DT_API_TOKEN: DT_API_TOKEN, DT_AGENT_BITNESS: 'all', DT_AGENT_PREFIX_DIR: '/tmp' }
+describe command(parse_cmd('~/paas-install.sh', opts)) do
   its(:stdout) { should match /Installing to \/tmp.*Unpacking complete./m }
   its(:stderr) { should match /^Connecting to ryx70518.dev.dynatracelabs.com./m }
   its(:exit_status) { should eq 0 }
@@ -173,8 +222,8 @@ end
 
 describe file('/tmp/dynatrace/oneagent/dynatrace-env.sh') do
   it { should be_file }
-  its(:content) { should include 'export DT_TENANT=ryx70518' }
-  its(:content) { should include 'export DT_TENANTTOKEN=NkM5fd7JG1Hzmmoh' }
+  its(:content) { should include 'export DT_TENANT=' + DT_TENANT }
+  its(:content) { should include 'export DT_TENANTTOKEN=' + DT_TENANTTOKEN }
   its(:content) { should match /export DT_CONNECTION_POINT=".+"/ }
 end
 
