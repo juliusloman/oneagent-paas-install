3f8882cc801027e1a28132f18ad2bb2d0c496255
Martin Etmajer <martin@etmajer.com>
2017-03-28 21:36:06 +0200

    Add integration for Apache HTTPD.

--- test/dynatrace/oneagent.rb
+++ test/dynatrace/oneagent.rb
@@ -7,6 +7,20 @@ module Dynatrace
       return "/tmp/dynatrace/oneagent/dynatrace-agent#{bitness}.sh #{cmd}"
     end
 
+    def self.get_monitored_bg_process_cmd(cmd, process = nil, lifetime = 10, bitness = Dynatrace::OneAgent::BITNESS_64)
+      if process.nil?
+        process = `basename #{cmd}`
+      end
+
+      return "((sleep #{lifetime} && killall #{process}) &); /tmp/dynatrace/oneagent/dynatrace-agent#{bitness}.sh #{cmd}"
+    end
+
+    class Apache
+      def self.get_monitored_process_log()
+        return "/tmp/dynatrace/oneagent/log/apache/ruxitagent_Apache_Web_Server_apache2_*.log"
+      end
+    end
+
     class Java
       def self.get_monitored_process_log()
         date = Time.now.strftime('%Y-%m-%d')
--- test/integration/oneagent-64-apache/serverspec/default_spec.rb
+++ test/integration/oneagent-64-apache/serverspec/default_spec.rb
@@ -8,11 +8,11 @@ require ENV['HOME'] + '/test/dynatrace/defaults.rb'
 require ENV['HOME'] + '/test/dynatrace/oneagent.rb'
 require ENV['HOME'] + '/test/dynatrace/util.rb'
 
-# Test installer: NGINX, 64-bit, into /tmp)
+# Test installer: Apache, 64-bit, into /tmp)
 opts = {
   DT_AGENT_BASE_URL:   Dynatrace::Defaults::DT_AGENT_BASE_URL,
   DT_API_TOKEN:        Dynatrace::Defaults::DT_API_TOKEN,
-  DT_AGENT_FOR:        'nginx',
+  DT_AGENT_FOR:        'apache',
   DT_AGENT_BITNESS:    '64',
   DT_AGENT_PREFIX_DIR: '/tmp'
 }
@@ -21,11 +21,12 @@ describe command(Dynatrace::Util::parse_cmd('~/paas-install.sh', opts)) do
   its(:exit_status) { should eq 0 }
 end
 
-describe command(Dynatrace::OneAgent::get_monitored_process_cmd('nginx')) do
+describe command(Dynatrace::OneAgent::get_monitored_bg_process_cmd('/opt/docker/bin/service.d/httpd.sh', 'apache2')) do
   its(:exit_status) { should eq 0 }
 end
 
 # Cannot use file() resource here, since only command() accepts a wildcard pattern
-describe command('cat ' + Dynatrace::OneAgent::NGINX::get_monitored_process_log) do
-  its(:stdout) { should match /Nginx successfully instrumented/ }
+describe command('cat ' + Dynatrace::OneAgent::Apache::get_monitored_process_log) do
+  its(:stdout) { should match /Loading agent/ }
+  its(:stdout) { should match /Agent name.*Apache.*/ }
 end
