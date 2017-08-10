a0e33b3ea4d04e60d0ea21f9eea89d81986176c2
Martin Etmajer <martin@etmajer.com>
2017-08-10 16:49:43 +0200

    Minor update.

--- test/integration/oneagent-64-nodejs/bootstrap.sh
+++ test/integration/oneagent-64-nodejs/bootstrap.sh
@@ -5,6 +5,4 @@ sudo apt-get install -y nodejs psmisc
 curl -L https://github.com/heroku/node-js-sample/archive/master.zip > /tmp/node-js-sample.zip
 unzip /tmp/node-js-sample.zip -d /
 mv /node-js-sample-master /app
-
-cd /app
-npm install
+cd /app; npm install
