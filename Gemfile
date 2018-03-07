179bb4ae9250a879d04527f30285a54a0ea2019b
esukram <markus@esukram.org>
2018-03-07 12:15:12 +0100

    Clean-up of repository, leaving only the further documentation.

--- Gemfile
+++
@@ -1,6 +0,0 @@
-source 'https://rubygems.org'
-
-group :integration do
-  gem 'kitchen-docker_cli'
-  gem 'test-kitchen'
-end
--- test/dynatrace/defaults.rb
+++
@@ -1,9 +0,0 @@
-module Dynatrace
-  class Defaults
-    DT_API_TOKEN = 'w89AycHAQByUUfu993UHG'
-    DT_API_URL = 'https://ryx70518.dev.dynatracelabs.com/api'
-    DT_CLUSTER_HOST = 'dev.dynatracelabs.com'
-    DT_TENANT = 'ryx70518'
-    DT_TENANT_TOKEN = 'NkM5fd7JG1Hzmmoh'
-  end
-end
